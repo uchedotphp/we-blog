a8c0579a1abca1891a9b2a8a918438f97962a888
uchedotphp <uchedotphp@gmail.com>
2020-08-11 10:12:47 +0100

    used Nprogress in router.beforeEach and afterEach

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Nprogress from "nprogress";
 
 Vue.use(VueRouter);
 
@@ -7,14 +8,23 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
-  }
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  Nprogress.start();
+  next();
+});
+
+router.afterEach(() => {
+  Nprogress.done();
 });
 
 export default router;
