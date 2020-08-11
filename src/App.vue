a5c005c49e1f78f643d4a2e30fd1a969f7251707
uchedotphp <uchedotphp@gmail.com>
2020-08-11 05:18:40 +0100

    added element ui plugin

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: "app",
+  components: {},
+};
+</script>
+
+<style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
-  },
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
